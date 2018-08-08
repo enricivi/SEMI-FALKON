f8167287546f2a50b1160d5f85cf5ecaa30c1eca
Enrico Civitelli <enrico.civitelli@gmail.com>
2018-08-08 14:56:34 +0200

    Modified train_falkon
    Now the train function strictly follows the paper algorithm

--- falkon.py
+++ falkon.py
@@ -2,6 +2,7 @@ import numpy as np
 from time import time
 from progressbar import progressbar as bar
 from numba import jit
+from copy import copy
 
 from utility.kernel import gaussian
 
@@ -18,17 +19,8 @@ def falkon(x_test, alpha, nystrom, gaussian_sigma):
 
 
 def train_falkon(x, y, m, gaussian_sigma, regularizer, max_iter):
-    """
-    :param x: is a matrix (size: n, features length) containing the train set
-    :param y: is an array of label
-    :param m: number of Nystrom centers (only uniform sampling)
-    :param kernel_function: define the kernel used during the computation
-    :param regularizer: lambda in objective function (see papers for more details)
-    :param max_iter: maximum iterations number during the optimization
-    :return: a vector of coefficients (size: m, 1) used for the future predictions
-    """
     start = time()
-    c, d = nystrom_centers(x, m)
+    c = nystrom_centers(x, m)
     print("  --> Nystrom centers selected in {:.3f} seconds".format(time()-start))
 
     start = time()
@@ -39,20 +31,16 @@ def train_falkon(x, y, m, gaussian_sigma, regularizer, max_iter):
     rank = np.linalg.matrix_rank(M=kmm, hermitian=True)
     if rank != m:
         print("  --> Rank deficient KMM ({} instead of {})".format(rank, m))
-    t = np.linalg.cholesky(a=((d @ kmm @ d) + (1e-5*m*np.eye(m))))  # 1e-5*m*eye is necessary because of numerical errors
+    t = np.linalg.cholesky(a=(kmm + (1e-5*m*np.eye(m))))
     a = np.linalg.cholesky(a=(((t @ t.T)/m) + regularizer*np.eye(m)))
     print("  --> Computed T and A in {:.3f} seconds".format(time()-start))
 
     start = time()
-    vec, kmn_knm = knm(vector=y, train=x, centroids=c, sigma=gaussian_sigma)
-    print("  --> Computed KNM matrices in {:.3f} seconds".format(time() - start))
-
-    start = time()
-    b = np.linalg.solve(a, np.linalg.solve(t, d @ vec)) / len(y)
+    b = np.linalg.solve(a, np.linalg.solve(t, kmn_vector(vec=y, train=x, nystrom=c, sigma=gaussian_sigma))) / len(y)
     print("  --> Computed b in {:.3f} seconds".format(time() - start))
 
     start = time()
-    beta = conjgrad(lambda _beta: bhb(beta=_beta, a=a, t=t, d=d, kmn_knm=kmn_knm/len(y), kmm=regularizer*kmm),
+    beta = conjgrad(lambda _beta: bhb(beta=_beta, a=a, t=t, train=x, nystrom=c, s=gaussian_sigma, lmb=regularizer),
                     b=b, max_iter=max_iter)
     print("  --> Optimization done in {:.3f} seconds".format(time() - start))
 
@@ -63,8 +51,8 @@ def train_falkon(x, y, m, gaussian_sigma, regularizer, max_iter):
 
 def nystrom_centers(x, m):
     c = x[np.random.choice(a=x.shape[0], size=m, replace=False), :]
-    d = np.diag(v=np.ones(shape=m))
-    return c, d
+    # d = np.diag(v=np.ones(shape=m))
+    return c
 
 
 @jit(nopython=True, nogil=True)
@@ -78,29 +66,41 @@ def kernel_matrix(points1, points2, sigma):
     return kernel_mtr
 
 
-@jit
-def knm(vector, train, centroids, sigma):
-    # computes KNM.T times KNM and KNM.T times vector
-    m = len(centroids)
-    n = len(train)
+@jit(nopython=True)
+def kmn_knm_vector(vec, train, nystrom, sigma):
+    m = len(nystrom)
 
-    vec = np.zeros(shape=m, dtype=np.float32)
-    kmn_knm = np.zeros(shape=(m, m), dtype=np.float32)
-    for i in bar(range(0, n, m)):
+    res = np.zeros(shape=m, dtype=np.float32)
+    for i in range(0, len(train), m):
         subset_train = train[i:i + m, :]
-        subset_vector = vector[i:i + m]
+        subset_vec = vec[i:i + m]
 
-        tmp_kernel_matrix = kernel_matrix(centroids, subset_train, sigma)
+        subset_knm = kernel_matrix(subset_train, nystrom, sigma)
 
-        vec += tmp_kernel_matrix @ subset_vector
-        kmn_knm += tmp_kernel_matrix @ tmp_kernel_matrix.T
+        res += (subset_knm.T @ (subset_knm @ subset_vec))
 
-    return vec, kmn_knm
+    return res
 
 
-def bhb(beta, a, t, d, kmn_knm, kmm):
-    w = (kmn_knm + kmm) @ d @ np.linalg.solve(t, np.linalg.solve(a, beta))
-    return np.linalg.solve(a.T, np.linalg.solve(t.T, d @ w))
+@jit
+def kmn_vector(vec, train, nystrom, sigma):
+    m = len(nystrom)
+
+    res = np.zeros(shape=m, dtype=np.float32)
+    for i in bar(range(0, len(train), m)):
+        subset_train = train[i:i + m, :]
+        subset_vec = vec[i:i + m]
+
+        subset_kmn = kernel_matrix(nystrom, subset_train, sigma)
+
+        res += (subset_kmn @ subset_vec)
+
+    return res
+
+
+def bhb(beta, a, t, train, nystrom, s, lmb):
+    _beta = kmn_knm_vector(vec=np.linalg.solve(t, np.linalg.solve(a, beta)), train=train, nystrom=nystrom, sigma=s)
+    return np.linalg.solve(a.T, np.linalg.solve(t.T, ((_beta / len(train)) + (lmb * np.linalg.solve(a, beta)))))
 
 
 def conjgrad(fun_w, b, max_iter):
